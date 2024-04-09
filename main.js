bc4678e402309332f67f8c553e673a47c445bd62
Alex Protchenko <p633442@gmail.com>
2024-04-09 18:08:37 +0300

    delete class btn-grey in html and update js

--- main.js
+++ main.js
@@ -1,5 +1,65 @@
 let display = document.querySelector('.display');
-let buttons = Array.from(document.querySelectorAll('button'));
+let buttons = document.querySelectorAll('button');
+let action = '';
+let numOne = '';
+let numTwo = '';
+let result = '';
+
+for (let e of buttons) {
+    e.addEventListener('click', () => {
+        if (!e.className.includes('btn-orange') && action === '') {
+            numOne += e.textContent;
+            display.textContent = numOne;
+        } else if (numOne !== '' && e.className.includes('btn-orange')) {
+            action = e.textContent;
+        } else if (numOne !== '' && action !== '') {
+            numTwo += e.textContent;
+            display.textContent = numTwo;
+        }
+        switch (action) {
+            case "AC":
+                display.textContent = "0";
+                numOne = '';
+                numTwo = '';
+                action = '';
+                break;
+            case "-":
+                result = numOne - numTwo;
+                break;
+            case "+":
+                result = Number(numOne) + Number(numTwo);
+                break;
+            case "=":
+                result = String(result);
+                if (result.length > 11) {
+                    result = result.slice(0, 11);
+                    display.textContent = result;
+                } else {
+                    display.textContent = result;
+                }
+                break;
+            case "*":
+                result = numOne * numTwo;
+                break;
+            case "/":
+                result = numOne / numTwo;
+                break;
+            case "%":
+                result = numOne - (numOne * numTwo / 100);
+                break;
+            case "√":
+                result = Math.sqrt(Number(numOne));
+                break;
+            default: 
+                break;
+        }
+    });
+}
+
+
+/* Варіант 2 */
+
+/* let buttons = Array.from(document.querySelectorAll('button'));
 
 buttons.map((button) => {
     button.addEventListener('click', (e) => {
@@ -13,7 +73,6 @@ buttons.map((button) => {
                 } catch (e) {
                     display.textContent = "Error!";
                 }
-                /* display.textContent = eval(display.textContent); */
                 break;
             case "%":
                 let passedText = display.textContent + "/100";
@@ -32,4 +91,4 @@ buttons.map((button) => {
         console.log(button);
         console.log(buttons);
     });
-});
+}); */
